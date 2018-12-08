e075db2f9cf7431143eb18f79833c16c042d2d07
Erick Vega <evega-12@hotmail.com>
2018-12-08 17:10:02 -0600

    Create sregistro.php

---
+++ sregistro.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Aplicacion de gestion de Sala de conferencias</title>
+</head>
+<body>
+
+
+<form action="sregistro.php" method="get">
+  <p>Nombre: <input type="text" name="nombre" size="40"></p>
+
+<form action="sregistro.php" method="get">
+  <p>Domicilio: <input type="text" name="nombre" size="40"></p>
+
+
+
+<?php
+            echo "";
+        ?>
+
+</body>
+</html>
